742d4598e19f7544185d1736ffd1e5d2e471a56a
Thetreck <luis.acv123@outlook.com>
2017-04-20 09:39:54 -0500

    cosa
    caos

---
+++ js/funcion.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+window.onload = function(){
+	var audio=document.getElementById("audio");
+	var au=document.getElementById("a2");
+	var jl=document.getElementById("2");
+	var xm=document.getElementById("3");
+	var kj=document.getElementById("4");
+	
+	document.getElementById("play").onclick = function(){audio.play()};
+	document.getElementById("play2").onclick = function(){a2.play()};
+	document.getElementById("p3").onclick = function(){jl.play()};
+	document.getElementById("p4").onclick = function(){xm.play()};
+	document.getElementById("p5").onclick = function(){kj.play()};
+};
